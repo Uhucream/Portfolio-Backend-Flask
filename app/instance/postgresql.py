55e19e2a71f368cb2094fb2799257552fd3c05a3
Uhucream <aborog1324@yahoo.co.jp>
2021-02-09 04:58:54 +0900

    微修正

--- app/instance/postgresql.py
+++ app/instance/postgresql.py
@@ -1,8 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
-    'user': os.getenv('RDS_USERNAME', 'postgres'),
-    'password': os.getenv('POSTGRES_PASSWORD', 'develop'),
-    'host': '{}:5432'.format(str(os.getenv('RDS_HOSTNAME') or os.getenv('DATABASE_URL'))),
-    'name': 'postgres'
-})
+if os.getenv('FLASK_CONFIG') == 'development':
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
+        'user': os.getenv('RDS_USERNAME', 'postgres'),
+        'password': os.getenv('POSTGRES_PASSWORD', 'develop'),
+        'host': '{}:5432'.format(str(os.getenv('RDS_HOSTNAME'))),
+        'name': 'postgres'
+    })
+else:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
