8f6ab9cd2f3b05cc4e51a625270d6a55db2a6b15
Uhucream <aborog1324@yahoo.co.jp>
2021-02-11 13:04:40 +0900

    微修正

--- app/instance/postgresql.py
+++ app/instance/postgresql.py
@@ -8,10 +8,11 @@ if os.getenv('FLASK_CONFIG') == 'development':
         'name': 'postgres'
     })
 elif os.getenv('RDS_HOSTNAME') is not None:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/{name}'.format(**{
-        'user': os.getenv('RDS_USERNAME', 'postgres'),
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'.format(**{
+        'user': os.getenv('RDS_USERNAME'),
         'password': os.getenv('RDS_PASSWORD'),
-        'host': '{}:{}'.format(str(os.getenv('RDS_HOSTNAME')), str(os.getenv('RDS_PORT'))),
+        'host': str(os.getenv('RDS_HOSTNAME')),
+        'port': str(str(os.getenv('RDS_PORT'))),
         'name': os.getenv('RDS_DB_NAME')
     })
 else:
