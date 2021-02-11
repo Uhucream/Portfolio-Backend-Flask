d0e570c66a1ffd38a97bd1218eda62437e236d18
Uhucream <aborog1324@yahoo.co.jp>
2021-02-11 13:30:46 +0900

    微修正

--- app/instance/postgresql.py
+++ app/instance/postgresql.py
@@ -2,11 +2,11 @@ import os
 
 if os.getenv('RDS_HOSTNAME') is not None:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'.format(**{
-        'user': os.getenv('RDS_USERNAME'),
-        'password': os.getenv('RDS_PASSWORD'),
+        'user': str(os.getenv('RDS_USERNAME')),
+        'password': str(os.getenv('RDS_PASSWORD')),
         'host': str(os.getenv('RDS_HOSTNAME')),
         'port': str(str(os.getenv('RDS_PORT'))),
-        'name': os.getenv('RDS_DB_NAME')
+        'name': str(os.getenv('RDS_DB_NAME'))
     })
 else:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
