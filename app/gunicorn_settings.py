7bff150ea585a83b0055cab2728b9815e003339d
Uhucream <aborog1324@yahoo.co.jp>
2021-02-11 15:48:22 +0900

    gunicornのログディレクトリ変更

--- app/gunicorn_settings.py
+++ app/gunicorn_settings.py
@@ -3,7 +3,7 @@ import socket
 
 bind = '0.0.0.0:{}'.format(str(os.getenv('PORT', 5000)))
 workers = 1
-accesslog = '/var/log/gunicorn/gunicorn.log'
+accesslog = '/var/log/flask/gunicorn.log'
 
 if str(os.getenv('FLASK_CONFIG')) == 'development':
     loglevel = 'debug'
