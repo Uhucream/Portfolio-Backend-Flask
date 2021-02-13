a57be6bd11baa5020ffd67799ff7e59e0df49b28
Uhucream <aborog1324@yahoo.co.jp>
2021-02-13 12:33:55 +0900

    ログ仕様を変更

--- app/gunicorn_settings.py
+++ app/gunicorn_settings.py
@@ -3,7 +3,7 @@ import socket
 
 bind = '0.0.0.0:{}'.format(str(os.getenv('PORT', 5000)))
 workers = 1
-accesslog = '/var/log/flask/gunicorn.log'
+accesslog = '-'
 
 if str(os.getenv('FLASK_CONFIG')) == 'development':
     loglevel = 'debug'
