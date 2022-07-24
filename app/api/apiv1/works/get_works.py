62dc63c59dba66fb87617f20b23b839835de46a7
Uhucream <aborog1324@yahoo.co.jp>
2022-07-25 00:40:38 +0900

    refactor: get_works.py の書き方をアーリーリターンの形式に

--- app/api/apiv1/works/get_works.py
+++ app/api/apiv1/works/get_works.py
@@ -19,16 +19,16 @@ def get_all_works():
             content, status_code, mimetype)
 
         return response
-    else:
-        converted_records_list = list(map(lambda record: record.to_dict(), record_list))
 
-        content = json.dumps(converted_records_list, ensure_ascii=False)
-        status_code = 200
-        mimetype = 'application/json;charset=UTF-8'
-        response = create_response.create_response(
-            content, status_code, mimetype)
+    converted_records_list = list(map(lambda record: record.to_dict(), record_list))
 
-        return response
+    content = json.dumps(converted_records_list, ensure_ascii=False)
+    status_code = 200
+    mimetype = 'application/json;charset=UTF-8'
+    response = create_response.create_response(
+        content, status_code, mimetype)
+
+    return response
 
 
 @api.route('/my_work/<endpoint_uri>', methods=['GET'])
@@ -45,13 +45,13 @@ def get_one_work(endpoint_uri=None):
             content, status_code, mimetype)
 
         return response
-    else:
-        result_dict = record.to_dict()
 
-        content = json.dumps(result_dict, ensure_ascii=False)
-        status_code = 200
-        mimetype = 'application/json;charset=UTF-8'
-        response = create_response.create_response(
-            content, status_code, mimetype)
+    result_dict = record.to_dict()
 
-        return response
+    content = json.dumps(result_dict, ensure_ascii=False)
+    status_code = 200
+    mimetype = 'application/json;charset=UTF-8'
+    response = create_response.create_response(
+        content, status_code, mimetype)
+
+    return response
